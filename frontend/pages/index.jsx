0cfae351427aac469487371ff09eab2d2afa9d15
Luciano <legarcia2@teco.com.ar>
2023-11-13 12:40:12 -0300

    Installing next.js

---
+++ frontend/pages/index.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <header>
+        <title>XCHAIN-DEX</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/frontend/public/vercel.svg" />
+      </header>
+      <main>
+        <p>XCHAIN-DEX</p>
+      </main>
+    </>
+  );
+}
